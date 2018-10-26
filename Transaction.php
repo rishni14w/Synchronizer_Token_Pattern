c2cbe5f0577ae131d3f62372b93e9ea1481470c1
Rishni <Rishni@DESKTOP-7681NTT>
2018-10-26 14:07:50 +0530

    modify website design

--- Transaction.php
+++ Transaction.php
@@ -27,15 +27,78 @@ window.addEventListener('load', function() {
 	<head>
 		<title>Transaction</title>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+    <style>
+      td{
+          padding: 20px;
+      }
+    </style>
 	</head>
-	<body>
-		<h1>Transaction</h1>
-		<form action="./classes/Transfer.php" method="post">
-			From Account: <input type="number" name="from"><br/> <br/>
-			To Account: <input type="number" name="to"><br/> <br/>
-			Amount: <input type="number" name="amount"><br/> <br/>
-			<input type="submit" id="submit_btn" value="Transfer">
-			</div>
-		</form>
+	<body style="background-color: LIGHTSALMON">
+    <div class="jumbotron text-center" style="margin-bottom:0;background-color: INDIANRED;color: white">
+      <div class="container" style="margin-top:30px" >
+          <div class="row">
+            <div class="col-sm-4" style="float: right">
+            
+          </div>
+          <div class="col-sm-6" style="float: left">
+            <h1 style="float: left;padding: 20px">R Bank</h1>
+          </div>
+            
+          </div>
+        </div>
+    </div>
+
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#" style="background-color: CRIMSON;color: white;font-size: larger;border-radius: 60%">Transaction</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact Us</a>
+          </li>    
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li> 
+        </ul>
+      </div>  
+    </nav>
+
+    <div class="container" style="margin-top:0px;background-color: LIGHTSALMON">
+      <div class="row">
+          <div class="col-sm-12">           
+          <h2 style="text-align: center;color: BLACK;">Transaction</h2> 
+          <form action="./classes/Transfer.php" method="post">
+            <table border="0" align="center" style="background-color: SEASHELL">
+              <tr>
+                    <td>From Account</td>
+                    <td><input type="number" name="from"/></td>
+                </tr>
+                <tr>
+                    <td>To Account</td>
+                    <td><input type="number" name="to"></td>
+                </tr>
+                <tr>
+                    <td>Amount</td>
+                    <td><input type="number" name="amount"></td>
+                </tr>
+                <tr>
+                  <td><input type="submit" class="btn btn-primary" id="submit_btn" value="Transfer"/></td>
+                </tr>                   
+            </table>
+          </form>
+        </div>
+      </div>
+    </div>
+    
 	</body>
 </html>
--- login.php
+++ login.php
@@ -2,15 +2,76 @@
 <html>
 	<head>
 		<title>Login</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+		<style>
+			td{
+			    padding: 20px;
+			}
+		</style>
 	</head>
-	<body>
-		<h1>Login</h1>
-		<form action="./classes/loginToken.php" method="post">
-			Username: <input type="text" name="username"><br/> <br/>
-			Password: <input type="password" name="Password"><br/> <br/>
-			<input type="submit" value="Login">
+	<body style="background-color: LIGHTSALMON">
+
+		<div class="jumbotron text-center" style="margin-bottom:0;background-color: INDIANRED;color: white">
+			<div class="container" style="margin-top:30px" >
+			  	<div class="row">
+				    <div class="col-sm-4" style="float: right">
+						
+					</div>
+					<div class="col-sm-6" style="float: left">
+						<h1 style="float: left;padding: 20px">R Bank</h1>
+					</div>
+			  		
+		  		</div>
+		  	</div>
+		</div>
+
+		<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+		  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+		    <ul class="navbar-nav">
+		      <li class="nav-item">
+		        <a class="nav-link" href="#" style="background-color: CRIMSON;color: white;font-size: larger;border-radius: 60%">Login</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Home</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Contact Us</a>
+		      </li>    
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">About</a>
+		      </li> 
+		    </ul>
+		  </div>  
+		</nav>
+
+		<div class="container" style="margin-top:0px;background-color: LIGHTSALMON">
+			<div class="row">
+			    <div class="col-sm-12">			    	
+					<h2 style="text-align: center;color: BLACK;">Login</h2> 
+					<form action="./classes/loginToken.php" method="post">
+						<table border="0" align="center" style="background-color: SEASHELL">
+							<tr>
+						        <td>Username</td>
+						        <td><input type="text" name="username"/></td>
+						    </tr>
+						    <tr>
+						        <td>Password</td>
+						        <td><input type="password" name="Password"></td>
+						    </tr>
+						    <tr>
+						    	<td><input type="submit" class="btn btn-primary" value="Login"/></td>
+						    </tr>						        
+						</table>
+					</form>
+				</div>
 			</div>
-		</form>
+		</div>
+		
 	</body>
 </html>
 
